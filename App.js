0cfc6e550ec40607f54a6c39b7486b72fd450c0c
Richard Obiri <obiriaframson@gmail.com>
2021-04-10 18:57:14 +0000

    norm

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={()=>alert("Hey ! why did you press me ")} style={styles.butt}>
+        <Text style={{color:'white'}}>Click This button</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  butt:{
+    padding:10, 
+    backgroundColor:'black',
+    width:300,
+    borderRadius:10,
+    alignItems:'center',
+    height:40,
+    justifyContent:'center'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
